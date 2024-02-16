33011ab134352ef0b2c66ce4e9861e239935849f
Gaic4o <alstntorl@naver.com>
2024-02-16 19:39:37 +0900

    feat(apps/minsu_dev): add header and footer components

--- apps/minsu_dev/src/app/layout.tsx
+++ apps/minsu_dev/src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+      <html lang={'ko'}>
+      <head/>
+      <body className="m-0">
+      <div className={`min-h-screen mx-auto bg-white ${montserrat.className} w-full mob:w-[880px]`}>
+          <div className="flex flex-1 flex-col">
+              <Header/>
+              {}
+              <Footer />
+          </div>
+      </div>
+      </body>
+      </html>
+);
 }
---
+++ apps/minsu_dev/src/components/layout/footer.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+    return (
+        <footer className="flex content-center justify-center p-4">
+            <a
+                href="https://github.com/Gaic4o"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg leading-5 font-bold text-black no-underline"
+            >
+                ©MinsuKim
+            </a>
+        </footer>
+    );
+};
+
+export default Footer;
+
+
