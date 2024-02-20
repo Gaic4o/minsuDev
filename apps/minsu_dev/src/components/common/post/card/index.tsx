ecf44a716bc6c8aba054a7ef393e820aae93cba2
Gaic4o <alstntorl@naver.com>
2024-02-20 14:09:40 +0900

    feat(apps/minsu_dev): Create PostCard Component

--- apps/minsu_dev/src/components/common/post/card/index.tsx
+++ apps/minsu_dev/src/components/common/post/card/index.tsx
@@ -8,6 +8,33 @@ type PostCardProps = Omit<Post, 'category' | 'tags'> & {
   className?: string;
 };
 
+const PostImage: FC<{ url: string; title: string }> = ({ url, title }) => (
+  <div
+    className="border border-[#e0e0e0] rounded-lg mt-10 relative overflow-hidden w-full mob:h-64"
+    style={{ aspectRatio: '1.8 / 1' }}
+  >
+    <Image src={url} layout="fill" objectFit="cover" alt={title} />
+  </div>
+);
+
+const PostContent: FC<{ title: string; date: string; content: string }> = ({
+  title,
+  date,
+  content,
+}) => (
+  <>
+    <h2 className="text-2xl text-thinGray100 font-bold mt-4 mb-2 break-words">
+      {title}
+    </h2>
+    <p className="text-sm text-thinGray300 mt-1.25 mb-0.5 mob:text-xs">
+      {date}
+    </p>
+    <p className="text-thinGray400 text-lg mt-2.5 mb-5 mob:text-base">
+      {content}
+    </p>
+  </>
+);
+
 const PostCard: FC<PostCardProps> = ({
   id,
   title,
@@ -15,9 +42,16 @@ const PostCard: FC<PostCardProps> = ({
   created_at,
   preview_image_url,
 }) => {
+  const formattedDate = format(new Date(created_at), 'yyyy년 M월 d일');
+
   return (
     <div className="w-full px-3 pb-7.5 border-b border-[#cecfd3] mob:w-[700px]">
-      <></>
+      <Link href={`/posts/${id}`} passHref>
+        <div className="cursor-pointer">
+          <PostImage url={preview_image_url ?? ''} title={title} />
+          <PostContent title={title} date={formattedDate} content={content} />
+        </div>
+      </Link>
     </div>
   );
 };
