32d86e455023f8b311c79bf1da54016b261d5315
Gaic4o <alstntorl@naver.com>
2024-02-24 01:19:15 +0900

    style(apps/minsu_dev): apply prettier settings.

--- apps/minsu_dev/src/components/layout/footer.tsx
+++ apps/minsu_dev/src/components/layout/footer.tsx
@@ -1,20 +1,18 @@
 import { FC } from 'react';
 
 const Footer: FC = () => {
-    return (
-        <footer className="flex content-center justify-center p-4">
-            <a
-                href="https://github.com/Gaic4o"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-lg leading-5 font-bold text-thinGray100 no-underline"
-            >
-                ©MinsuKim
-            </a>
-        </footer>
-    );
+  return (
+    <footer className="flex content-center justify-center p-4">
+      <a
+        href="https://github.com/Gaic4o"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-lg leading-5 font-bold text-thinGray100 no-underline"
+      >
+        ©MinsuKim
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
-
-
--- apps/minsu_dev/src/foo/login/loginForm.tsx
+++ apps/minsu_dev/src/foo/login/loginForm.tsx
@@ -1,56 +1,66 @@
 'use client';
 
-import {Fragment} from "react";
-import {createClient} from "@/utils/supabase/client";
+import { Fragment } from 'react';
+import { createClient } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
 interface LoginFormInputs {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
-const supabase = createClient()
+const supabase = createClient();
 const LoginForm = () => {
-    const router = useRouter();
-    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInputs>();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>();
 
+  const onSubmit = async (data: LoginFormInputs) => {
+    const response = await supabase.auth.signInWithPassword({
+      email: data.email,
+      password: data.password,
+    });
 
-    const onSubmit = async (data: LoginFormInputs) => {
-        const response = await supabase.auth.signInWithPassword({
-            email: data.email,
-            password: data.password,
-        });
+    if (!response.data.user) {
+      return alert('로그인에 실패했습니다.');
+    }
 
-        if (!response.data.user) {
-            return alert('로그인에 실패했습니다.');
-        }
+    router.refresh();
+  };
 
-        router.refresh();
-    };
-
-
-    return (
-        <Fragment>
-            <div className={'flex flex-col gap-8'}>
-                <h1 className={'text'}>로그인</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                        <input type='text' placeholder={'이메일'} {...register('email', {required: true})}/>
-                        <input type={'password'} placeholder={'비밀번호'} {...register('password', {required: true})}/>
-                    </div>
-                    {errors.email && <span>이메일을 입력해주세요.</span>}
-                    {errors.password && <span>비밀번호를 입력해주세요.</span>}
-                    <button
-                        type={'submit'}
-                        className="mt-4 w-full rounded-md bg-gray-800 py-2 text-white"
-                    >
-                        로그인
-                    </button>
-                </form>
-            </div>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <div className={'flex flex-col gap-8'}>
+        <h1 className={'text'}>로그인</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input
+              type="text"
+              placeholder={'이메일'}
+              {...register('email', { required: true })}
+            />
+            <input
+              type={'password'}
+              placeholder={'비밀번호'}
+              {...register('password', { required: true })}
+            />
+          </div>
+          {errors.email && <span>이메일을 입력해주세요.</span>}
+          {errors.password && <span>비밀번호를 입력해주세요.</span>}
+          <button
+            type={'submit'}
+            className="mt-4 w-full rounded-md bg-gray-800 py-2 text-white"
+          >
+            로그인
+          </button>
+        </form>
+      </div>
+    </Fragment>
+  );
+};
 
 export default LoginForm;
