d757cfc4f99f27371dfd39999bd1d3875db1f2ba
Gaic4o <alstntorl@naver.com>
2024-02-24 01:10:40 +0900

    feat(apps/minsu_dev): add post logic

--- apps/minsu_dev/src/app/page.tsx
+++ apps/minsu_dev/src/app/page.tsx
@@ -1,9 +1,18 @@
-import PostList from "@/components/common/post/list";
+import PostList from '@/components/common/post/list';
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient(cookies());
+  const { data } = await supabase.from('Post').select('*');
   return (
-      <section className="flex flex-col items-center">
-        <PostList/>
-      </section>
+    <section className="flex flex-col items-center">
+      <PostList
+        initalPosts={data?.map(post => ({
+          ...post,
+          tags: JSON.parse(post.tags) as string[],
+        }))}
+      />
+    </section>
   );
 }
