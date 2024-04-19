ed889a41467385dccac86b5d2a0046be5e59a98b
Gaic4o <alstntorl@naver.com>
2024-04-19 09:34:30 +0900

    chore: login in the write folder was separated into the login folder. And I modified it to loginForm.

--- apps/minsu_dev/src/app/write/page.tsx
+++ apps/minsu_dev/src/app/write/page.tsx
@@ -1,11 +1,11 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
-import { Login } from '@/foo/write/login';
+import { LoginForm } from '@/foo/login/loginForm';
 import { WriteForm } from '@/foo/write/writeForm.tsx';
 
 export default async function Write() {
   const supabase = createClient(cookies());
   const userResponse = await supabase.auth.getUser();
 
-  return <>{!!userResponse?.data.user ? <WriteForm /> : <Login />}</>;
+  return <>{!!userResponse?.data.user ? <WriteForm /> : <LoginForm />}</>;
 }
